c2f3fb362197365e7d5dd4046429a24840533ac6
Bansode <suryakumar.bansode@atos.net>
2022-06-08 16:39:14 +0100

    Revert "New changes"
    This reverts commit 3c02c6c6d29a501fe57147998fe45353621f9373.

--- features/pageobjects/page.ts
+++ features/pageobjects/page.ts
@@ -1,10 +1,10 @@
 /**
 * main page object containing all methods, selectors and functionality
-* that is shared across all page objects - Pull example
+* that is shared across all page objects
 */
 export default class Page {
     /**
-    * Opens a sub page of the page - testing
+    * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     public open (path: string) {
